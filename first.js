ad8b6df22af139a36c9c02494717d809fd8b700b
aakashDeveloper <ahanda205@gmail.com>
2020-06-18 22:35:16 +0530

    a

---
+++ first.js
@@ -0,0 +1,65 @@
+Es5
+Es6
+Es7
+Ecma Script
+
+///////////////
+    Datatype
+////////////////
+String = "delhi" ,'Paris' ,'10', "true","30"
+Number = 454, 4434563465, 43.44
+Boolean = true/false
+null
+undefined
+
+
+///////////////
+var a = "Hi"
+var b = 20
+var c = true
+undefined
+a
+"Hi"
+typeof(a)
+"string"
+typeof(b)
+"number"
+typeof(c)
+"boolean"
+var d = "2423534"
+undefined
+var c = 352.246
+undefined
+typeof(c)
+"number"
+typeof(d)
+"string"
+
+
+////////////
+var a = 10
+var b = 2
+a+b
+12
+a-b
+8
+a*b
+20
+a/2
+5
+a%b
+0
+
+4%10
+5%10
+10%11
+11%10
+
+4%10
+4
+5%10
+5
+10%11
+10
+11%10
+1
---
+++ second.js
@@ -0,0 +1,53 @@
+var a = "Hi"
+var b=" JavaScript"
+undefined
+a+b
+"Hi JavaScript"
+var c =10
+undefined
+a+c
+"Hi10"
+c+a
+"10Hi"
+a-b
+NaN
+a*b
+NaN
+a/b
+NaN
+
+string+string = string
+string+number = string
+number+string = string
+number+number = number
+
+10+"10"+20
+"101020"
+10+10+"20"
+"2020"
+"20"+10+10
+"201010"
+
+10+"10"+20-1
+101019
+10+10+"20"-1
+2019
+"20"+10+10-1
+201009
+
+
+
+"10"+"10"
+"1010"
+"a"-"10"
+NaN
+"1"-"10"
+-9
+"10"*"a"
+NaN
+"10"*"22"
+220
+"10"/"22"
+0.45454545454545453
+"10"%"22"
+10
