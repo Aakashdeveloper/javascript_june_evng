050ed45bdba907223d60e34ab4525c2e12b21e65
aakashDeveloper <ahanda205@gmail.com>
2020-06-22 22:48:04 +0530

    a

---
+++ six.js
@@ -0,0 +1,17 @@
+= assignment
+== compare data
+=== compare data and datatype
+
+var city = "amsterdam"
+
+undefined
+city
+"amsterdam"
+var a = 10
+undefined
+var b = "10"
+undefined
+a==b
+true
+a===b
+false
